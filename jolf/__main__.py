2baa3513302e9b2a855e9e52c90f4de2da619a35
Saahil Ognawala <saahil.in@gmail.com>
2018-08-31 16:56:26 +0200

    before adding srg folks

--- jolf/__main__.py
+++ jolf/__main__.py
@@ -6,6 +6,8 @@ import os, sys, time, glob
 from config import AFL_FUZZ, KLEE
 import subprocess
 
+PREFIXES = ["/home/ognawala/coreutils-8.30/", "/home/ognawala/coreutils-8.30-gcov/"]
+
 def call_afl(max_time, seed_inputs_dir, output_dir, afl_object, argv):
     timeout = ["timeout", "--preserve-status", str(max_time)+"s"]
     afl_command = [AFL_FUZZ, "-i", seed_inputs_dir, "-o", output_dir, afl_object, argv, "@@"]
@@ -50,6 +52,63 @@ def clean_argv(argv):
 
     return clean
 
+def get_afl_command_args(d):
+    fuzzer_stats = open(os.path.join(d, "fuzzer_stats"), "r")
+    
+    command_args = ""
+
+    command_line = ""
+    for l in fuzzer_stats:
+        if l.startswith("command_line"):
+            command_line = l
+
+    toks = command_line.strip().split()
+    
+    i = len(toks)-1
+
+    if not toks[i]=="@@":
+        return command_args
+
+    i -= 1
+
+    while i>=0 and toks[i].startswith("-"):
+       command_args += " " + toks[i]
+       i -= 1
+
+    return command_args
+
+def get_afl_coverage(coverage_list, afl_out_dir):
+    coverage_file = open(os.path.join(afl_out_dir, "cov/id-delta-cov"), "r")
+    
+    for line in coverage_file:
+        if line.startswith("#"):
+            continue
+        fields = line.strip().split(", ")
+        if not fields[2].startswith(PREFIXES[1]):
+            continue
+        if not fields[3]=="line":
+            continue
+        file_name = fields[2].split(PREFIXES[1])[-1]
+        line_no = int(fields[4])
+
+        if (file_name, line_no) not in coverage_list:
+            coverage_list.append((file_name, line_no))
+
+    return coverage_list
+
+def get_klee_coverage(coverage_list, klee_out_dir):
+    for f in glob.glob(klee_out_dir+"/*.cov"):
+        cov_file = open(f, "r")
+        for line in cov_file:
+            if not line.startswith(PREFIXES[0]):
+                continue
+            file_name = line.strip().split(":")[0].split(PREFIXES[0])[-1]
+            line_no = int(line.strip().split(":")[-1])
+            if (file_name, line_no) not in coverage_list:
+                coverage_list.append((file_name, line_no))
+
+    return coverage_list
+
 def main():
     parser = argparse.ArgumentParser(description="AFL+KLEE flipper")
     parser.add_argument("-t", "--max-time-each", help="Max time(sec) allowed for each round of KLEE or AFL")
@@ -57,9 +116,34 @@ def main():
     parser.add_argument("-o", "--all-output-dir", help="Folder to run experiments in")
     parser.add_argument("-k", "--klee-object", help="Bitcode for KLEE")
     parser.add_argument("-b", "--afl-object", help="Binary or LLVM IR for AFL")
+    parser.add_argument("-c", "--coverage-mode", help="Run Jolf in coverage mode (results existing)")
+    parser.add_argument("-g", "--coverage-source", help="Location of project compiled with gcov")
+    parser.add_argument("-e", "--coverage-executable", help="Name of executable compiled with Gcov")
     
     args = parser.parse_args()
     seed_inputs_dir = os.path.join(args.all_output_dir, "all_seeds/")
+
+    if args.coverage_mode=="True":
+        print("Calculating coverage...")
+        fuzzing_dirs = glob.glob(args.all_output_dir+"/fuzzing-*") + [args.all_output_dir+"/init-fuzzing/"]
+        
+        for d in fuzzing_dirs:
+            if os.path.isdir(d+"/cov"):
+                continue
+            afl_command_args = get_afl_command_args(d)
+            os.system("afl-cov -d %s --coverage-cmd \"%s %s AFL_FILE\" --code-dir %s --coverage-include-lines"%(d, args.coverage_executable, afl_command_args, args.coverage_source))
+
+        coverage_list = []
+        
+        coverage_list = get_klee_coverage(coverage_list, args.all_output_dir+"/klee0")
+        
+        for d in fuzzing_dirs:
+            coverage_list = get_afl_coverage(coverage_list, d)
+
+        coverage_list = get_klee_coverage(coverage_list, args.all_output_dir+"/klee1")
+
+        print("Covered lines: %d"%(len(coverage_list)))
+        sys.exit(0)
     
     # Prepare directory
     if not os.path.isdir(args.all_output_dir):
