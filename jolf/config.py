27199f48019554632a49cd389d7dfded59c3a067
Saahil Ognawala <saahil.in@gmail.com>
2019-02-15 08:11:46 +0100

    no more prefixes

--- jolf/config.py
+++ jolf/config.py
@@ -1,3 +1,2 @@
 AFL_FUZZ = "afl-fuzz"
 KLEE = "/home/ognawala/build/klee-afl/Release+Asserts/bin/klee"
-PREFIXES = ["/home/ognawala/jolf-experiments/coreutils-8.30/", "/home/ognawala/jolf-experiments/coreutils-8.30-gcov/"]
